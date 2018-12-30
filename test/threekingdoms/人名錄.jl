4685169a6cd8db8edd49dbf5cdce0c9ee1323b1f
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-31 03:06:43 +0900

    字

--- test/threekingdoms/人名錄.jl
+++ test/threekingdoms/人名錄.jl
@@ -3,10 +3,12 @@ module test_threekingdoms_人名錄
 using ThreeKingdoms: 人名錄
 using Test
 
+@test 人名錄.賈詡 == 人名錄.登場人物["賈詡"] == (名 = "賈詡", 字 = "文和", 이름 = "가후", 자 = "문화")
 @test 人名錄.賈詡.이름 == "가후"
-@test 人名錄.登場人物["賈詡"] == 人名錄.賈詡
-@test findfirst(人名錄.登場人物) do 인물
-    인물.이름=="가후"
-end == "賈詡" 
+
+JiaXu = findfirst(人名錄.登場人物) do 인물
+    인물.이름== "가후"
+end
+@test JiaXu == "賈詡"
 
 end # module test_threekingdoms_人名錄
