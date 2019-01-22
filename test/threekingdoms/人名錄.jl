5386941898c4c7847603548301ae2198adda6925
WooKyoung Noh <wookay.noh@gmail.com>
2019-01-23 02:29:45 +0900

    English Name

--- test/threekingdoms/人名錄.jl
+++ test/threekingdoms/人名錄.jl
@@ -3,12 +3,17 @@ module test_threekingdoms_人名錄
 using ThreeKingdoms: 人名錄
 using Test
 
-@test 人名錄.賈詡 == 人名錄.登場人物["賈詡"] == (名 = "賈詡", 字 = "文和", 이름 = "가후", 자 = "문화")
+@test 人名錄.賈詡 == 人名錄.登場人物["賈詡"] == (名 = "賈詡", 字 = "文和", 이름 = "가후", 자 = "문화", Name = "Jia Xu")
 @test 人名錄.賈詡.이름 == "가후"
 
 JiaXu = findfirst(人名錄.登場人物) do 인물
     인물.이름 == "가후"
 end
 @test JiaXu == "賈詡"
+
+ZhaoYun = findfirst(人名錄.登場人物) do 인물
+    haskey(인물, :Name) && 인물.Name == "Zhao Yun"
+end
+@test ZhaoYun == "趙雲"
 
 end # module test_threekingdoms_人名錄
